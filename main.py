c10b275ef20593521b8604b910a72c83b417072d
happyguest666top <145270176+happyguest666top@users.noreply.github.com>
2023-09-24 17:18:58 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -10,19 +10,27 @@ text = QLabel(win)
 text.setText("Натисніть, щоб дізнатися переможця")
 text.move(70, 10)
 
+text1 = QLabel(win)
+text1.setText("?")
+text1.move(190, 70)
+
 text2 = QLabel(win)
 text2.setText("?")
-text2.move(190, 70)
+text2.move(190, 100)
 
 button = QPushButton(win)
-button.setText("Згенерувати")
+button.setText("Нажмі")
 button.move(140, 130)
 
 def show_win():
     number = randint(1, 100)
     text2.setText(str(number))
-    text.setText("Переможець:")
+    text.setText("Ура победа:")
 
+    number1 = randint(1, 100)
+    text1.setText(str(number1))
+    text.setText("Ура победа:")
+    text.setText("Ура победа:")
 button.clicked.connect(show_win)
 
 win.show()
